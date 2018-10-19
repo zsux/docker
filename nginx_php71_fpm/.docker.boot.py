61859bc96f32dc82cbe84cbbdcaed775d5e97ea9
dhole <dholer@63.com>
2018-10-19 23:17:20 +0800

    cs deploy

--- nginx_php71_fpm/.docker.boot.py
+++ nginx_php71_fpm/.docker.boot.py
@@ -34,19 +34,19 @@ def init_user():
             t = key.split("_")
             username = t[0]
             if username == "root":
-                os_system("mkdir -p /root/.ssh")
-                os_system("echo {0} >> /root/.ssh/authorized_keys".format(os.environ[env_key]))
-                os_system("chmod 600 /root/.ssh/authorized_keys")
+                os_system("sudo mkdir -p /root/.ssh")
+                os_system("sudo echo {0} >> /root/.ssh/authorized_keys".format(os.environ[env_key]))
+                os_system("sudo chmod 600 /root/.ssh/authorized_keys")
             else:
-                os_system("useradd -G sudo -m -s '/bin/bash' {}".format(username))
-                os_system("echo '{0}:{0}kd123456' | chpasswd".format(username))
-                os_system("mkdir -p /home/{0}/.ssh".format(username))
-                os_system("echo {0} >> /home/{1}/.ssh/authorized_keys".format(os.environ[env_key], username))
-                os_system("chmod 600 /home/{0}/.ssh/authorized_keys".format(username))
-                os_system("chown -R {0}:{0} /home/{0}/.ssh".format(username))
+                os_system("sudo useradd -G sudo -m -s '/bin/bash' {}".format(username))
+                os_system("sudo echo '{0}:{0}kd123456' | chpasswd".format(username))
+                os_system("sudo mkdir -p /home/{0}/.ssh".format(username))
+                os_system("sudo echo {0} >> /home/{1}/.ssh/authorized_keys".format(os.environ[env_key], username))
+                os_system("sudo chmod 600 /home/{0}/.ssh/authorized_keys".format(username))
+                os_system("sudo chown -R {0}:{0} /home/{0}/.ssh".format(username))
 
 if len(wwwroot) > 0:
-    cmd = "sed -i 's/root \/code/root {}/g' /etc/nginx/sites-enabled/default.conf".format(wwwroot.replace("/",'\/'));
+    cmd = "sudo sed -i 's/root \/code/root {}/g' /etc/nginx/sites-enabled/default.conf".format(wwwroot.replace("/",'\/'));
     print cmd
     os_system(cmd)
 
@@ -68,14 +68,14 @@ for item in boot.split(","):
     if item == "init-user":
         init_user()
     else:
-        os_system("cp /etc/supervisor/conf_d/{0}.conf /etc/supervisor/conf.d/{0}.conf".format(item))
+        os_system("sudo cp /etc/supervisor/conf_d/{0}.conf /etc/supervisor/conf.d/{0}.conf".format(item))
 
 if len(auth) > 0:
     if os.path.exists("/opt/init_webshell") == False:
         print "init_webshell"
-        os_system("touch /opt/init_webshell")
-        os_system("sh /opt/scripts/bin/webshell.sh {0} {1}".format(auth.split(":")[0],auth.split(":")[1]))
+        os_system("suod touch /opt/init_webshell")
+        os_system("sudo sh /opt/scripts/bin/webshell.sh {0} {1}".format(auth.split(":")[0],auth.split(":")[1]))
 
 if start == '1':
     print "starting"
-    os_system("/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf")
+    os_system("sudo /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf")
